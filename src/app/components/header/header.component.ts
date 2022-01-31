7563d8ebf519ce955f57b57bed5158b49c2231f4
Rahul Rajpal <rahulrajpal2911@gmail.com>
2022-02-01 00:39:33 +0530

    Website with static data

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,28 +5,28 @@ import { HandlerService } from 'src/app/services/handler.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
   dropdown = false;
   pageTitle = '';
   isHomePage = false;
+  showMenu = false;
 
   subscriptions: Subscription[] = [];
 
-  constructor(private handler: HandlerService) {
-  }
+  constructor(private handler: HandlerService) {}
 
   ngOnInit(): void {
-    const pageTitleSubscription = this.handler.pageTitle.subscribe(pageTitle => {
-      this.pageTitle = pageTitle;
-    });
+    const pageTitleSubscription = this.handler.pageTitle.subscribe(
+      (pageTitle) => {
+        this.pageTitle = pageTitle;
+      }
+    );
     this.subscriptions.push(pageTitleSubscription);
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
-
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,16 +5,17 @@ import { HandlerService } from 'src/app/services/handler.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute, private handlerService: HandlerService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private handlerService: HandlerService
+  ) {}
 
   ngOnInit(): void {
-    this.route.data.subscribe(data => {
+    this.route.data.subscribe((data) => {
       this.handlerService.updateTitle(data['pageTitle']);
-    })
+    });
   }
-
 }
